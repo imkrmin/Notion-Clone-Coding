d4eb97f2f29da450a85621ce0c59b1bd7e2d37a2
imkrmin <zum2n2@gmail.com>
2025-08-14 10:11:22 +0900

    초기 셋팅

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Pretendard", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [],
+};
